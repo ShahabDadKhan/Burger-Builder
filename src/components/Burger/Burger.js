b1d32d666e4e64f7e1e3e701e0ff381094acdc09
shahabdadkhan <shahabkhan@fynd-external.com>
2022-06-05 22:37:09 +0530

    completed

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  console.log("Props", props);
-  const transformedIngredients = Object.keys(props.ingredients).map((igkey) => {
-    return [...Array(props.ingredients[igkey])].map((_, i) => {
-      return <BurgerIngredient key={igkey + i} type={igkey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igkey) => {
+      return [...Array(props.ingredients[igkey])].map((_, i) => {
+        return <BurgerIngredient key={igkey + i} type={igkey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding the Ingredients</p>;
+  }
 
   return (
     <div className="Burger">
